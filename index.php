1ba0f398da6fc26c102acca19672294e0f79d574
Fernando Sánchez <fgsl.182@gmail.com>
2018-12-28 14:30:40 -0600

    Primer diseño
    Carta mediana, (Gold, UCL Live)

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/cards.css">
+    <title>FIFA 19</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="card card-md ucl-live">
+        <img class="player" src="assets/players/158023.png">
+        <img class="nation" src="assets/nations/52.png">
+        <img class="badge" src="assets/badges/241.png">
+        <div class="rating">
+          99
+        </div>
+        <div class="position">
+          cam
+        </div>
+        <div class="divider-nat"></div>
+        <div class="divider-bad"></div>
+        <div class="name">
+          messi
+        </div>
+        <div class="divider-nam"></div>
+        <div class="divider-att"></div>
+        <div class="pace">
+          99
+        </div>
+        <div class="shooting">
+          98
+        </div>
+        <div class="passing">
+          98
+        </div>
+        <div class="dribbling">
+          98
+        </div>
+        <div class="defending">
+          98
+        </div>
+        <div class="physical">
+          98
+        </div>
+        <div class="divider-che"></div>
+      </div>
+    </div>
+
+  </body>
+</html>
