c01a42cc933b9c4bd75c170d10f320946595f43f
Fernando Sánchez <fgsl.182@gmail.com>
2018-12-30 20:15:56 -0600

    Cartas 75% (swap deals)

--- index.php
+++ index.php
@@ -9,22 +9,27 @@
     <div class="container">
       <label for="card-type">Carta: </label>
       <select class="" id="card-type" name="card-type">
-        <optgroup label="-------------">
+        <optgroup>
           <option value="gold-rare">Oro raro</option>
           <option value="silver-rare">Plata raro</option>
           <option value="bronze-rare">Bronce raro</option>
         </optgroup>
-        <optgroup label="-------------">
+        <optgroup>
           <option value="gold-common">Oro comun</option>
           <option value="silver-common">Plata comun</option>
           <option value="bronze-common">Bronce comun</option>
         </optgroup>
-        <optgroup label="-------------">
+        <optgroup>
           <option value="gold-if">Oro IF</option>
           <option value="silver-if">Plata IF</option>
           <option value="bronze-if">Bronce IF</option>
         </optgroup>
-        <optgroup label="-------------">
+        <optgroup>
+          <option value="gold-fut-champions">Oro IF (FUT Champions)</option>
+          <option value="silver-fut-champions">Plata IF (FUT Champions)</option>
+          <option value="bronze-fut-champions">Bronce IF (FUT Champions)</option>
+        </optgroup>
+        <optgroup>
           <option value="ucl-rare">UCL raro</option>
           <option value="ucl-common">UCL comun</option>
           <option value="ucl-live">UCL live</option>
@@ -32,12 +37,44 @@
           <option value="ucl-sbc">UCL SBC</option>
           <option value="ucl-tott">UCL TOTT</option>
         </optgroup>
-        <optgroup label="-------------">
+        <optgroup>
           <option value="uel">UEL</option>
           <option value="uel-live">UEL live</option>
           <option value="uel-motm">UEL MOTM</option>
           <option value="uel-sbc">UEL SBC</option>
           <option value="uel-tott">UEL TOTT</option>
+        </optgroup>
+        <optgroup>
+          <option value="flashback">Flashback SBC</option>
+          <option value="futtie">Futtie</option>
+          <option value="futmas">FUTMAS</option>
+          <option value="hero">Hero</option>
+          <option value="icon">Icon</option>
+          <option value="motm">MOTM</option>
+          <option value="otw">Ones To Watch</option>
+          <option value="pro">Pro Player</option>
+          <option value="premium-sbc">Premium SBC</option>
+          <option value="record-breaker">Record Breaker</option>
+          <option value="sbc">SBC</option>
+        </optgroup>
+        <optgroup>
+          <option value="premier-league-potm">Premier League POTM</option>
+          <option value="bundesliga-potm">Bundesliga POTM</option>
+          <option value="award-winner">Award Winner</option>
+        </optgroup>
+        <optgroup>
+          <option value="swap-deals-1">Swap Deals 1</option>
+          <option value="swap-deals-2">Swap Deals 2</option>
+          <option value="swap-deals-3">Swap Deals 3</option>
+          <option value="swap-deals-4">Swap Deals 4</option>
+          <option value="swap-deals-5">Swap Deals 5</option>
+          <option value="swap-deals-6">Swap Deals 6</option>
+          <option value="swap-deals-7">Swap Deals 7</option>
+          <option value="swap-deals-8">Swap Deals 8</option>
+          <option value="swap-deals-9">Swap Deals 9</option>
+          <option value="swap-deals-10">Swap Deals 10</option>
+          <option value="swap-deals-11">Swap Deals 11</option>
+          <option value="swap-deals-reward">Swap Deals Reward</option>
         </optgroup>
       </select>
       <label for="size" style="padding-left:15px">Tamaño: </label>
@@ -49,12 +86,12 @@
       <br>
 
       <div id="card1" class="card card-lg gold-rare" style="margin-top:10px">
-        <!--<div class="divider-special"></div>-->
-        <img class="player" src="assets/players/158023.png">
-        <img class="nation" src="assets/nations/52.png">
-        <img class="badge" src="assets/badges/241.png">
+        <div class="divider-special"></div>
+        <img class="player" src="assets/players/test.png">
+        <img class="nation" src="assets/nations/83.png">
+        <img class="badge" src="assets/badges/1878.png">
         <div class="rating">
-          94
+          75
         </div>
         <div class="position">
           cam
@@ -62,27 +99,27 @@
         <div class="divider-nat"></div>
         <div class="divider-bad"></div>
         <div class="name">
-          messi
+          alvarado
         </div>
         <div class="divider-nam"></div>
         <div class="divider-att"></div>
         <div class="pace">
-          88
+          82
         </div>
         <div class="shooting">
-          91
+          69
         </div>
         <div class="passing">
-          88
+          72
         </div>
         <div class="dribbling">
-          96
+          79
         </div>
         <div class="defending">
-          32
+          19
         </div>
         <div class="physical">
-          61
+          54
         </div>
         <div class="divider-che"></div>
       </div>
